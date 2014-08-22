9a412b9a2d14a652caeaa22ce54f771382ccb1d4
cliffordp <tko@tourkick.com>
2014-08-22 09:19:32 -0500

    v1.1
    Changed to dual-width section instead of 2 stand-alone sections.
    Updated art.

--- bigtextdms.php
+++ bigtextdms.php
@@ -7,7 +7,7 @@ Plugin URI: http://www.pagelinestheme.com/bigtextdms-section?utm_source=pageline
 Description: A <a href="https://github.com/zachleat/BigText" target="_blank">BigText</a> section that resizes text to fit one or more words on a line that fits the container. Responsive too, which means it scales with different size browsers.
 Demo: http://www.pagelinestheme.com/bigtextdms-section?utm_source=pagelines&utm_medium=section&utm_content=demolink&utm_campaign=bigtextdms_section
 Class Name: BigTextDMS
-Version: 1.0
+Version: 1.1
 PageLines: true
 v3: true
 */
